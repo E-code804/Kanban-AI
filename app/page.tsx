6b7004b36e45b22e616121aa0c404e4071c480f7
Erik Pfeffer <epfeffer@terpmail.umd.edu>
2025-06-03 16:39:44 -0400

    Put auth logic into auth.ts in root level.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/auth";
 import { getServerSession } from "next-auth/next";
 import Image from "next/image";
 import { redirect } from "next/navigation";
